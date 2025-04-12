5e3bb601ed7c8a63f6d55d615f3077b955d9fd42
punchlemon <tsukiwahigashini0410@gmail.com>
2025-04-12 13:46:28 +0900

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by al dente on 2025/04/12.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
