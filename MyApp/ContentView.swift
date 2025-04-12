c8657fd99b79ebf6c62a2b322ec3842630e72eed
punchlemon <tsukiwahigashini0410@gmail.com>
2025-04-12 13:46:28 +0900

    Initial Commit

---
+++ MyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by al dente on 2025/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyAppTests/MyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppTests.swift
+//  MyAppTests
+//
+//  Created by al dente on 2025/04/12.
+//
+
+import Testing
+@testable import MyApp
+
+struct MyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
